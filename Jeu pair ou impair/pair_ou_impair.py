5ea6d4cee8dbee902fcc36c2ca8206625219b98d
Parn <83118843+falaerm@users.noreply.github.com>
2021-04-25 12:53:40 +0200

    Add files via upload

---
+++ Jeu pair ou impair/pair_ou_impair.py
@@ -0,0 +1,5 @@
+chiffre = float(input("Insérer un chiffre"))
+if chiffre % 2 == 0 :
+  print("Pair")
+else:
+  print("Impair")
---
+++ Jeu pair ou impair/pair_ou_impair_2.py
@@ -0,0 +1,7 @@
+liste_numéro = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+pair = []
+for i in liste_numéro:
+	if (i % 2) == 0:
+		pair.append(i)
+print("les nombres pairs sont: ", pair)
+
